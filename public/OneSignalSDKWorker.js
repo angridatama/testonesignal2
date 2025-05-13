8ff283d1dcabdbe7f42039c18dd8bc92e0ee5dd1
anggriawanmr <anggriawanmr@gmail.com>
2025-05-13 22:59:59 +0700

    Adding OneSignal

--- public/OneSignalSDKWorker.js
+++ public/OneSignalSDKWorker.js
@@ -1,2 +1,3 @@
 // Required Service Worker file for OneSignal
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.worker.js');
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
