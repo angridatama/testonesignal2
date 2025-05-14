bc157c37f7278192262a2c50a768721e3192728b
anggriawanmr <anggriawanmr@gmail.com>
2025-05-14 19:49:07 +0700

    Removing the tailwind

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import './output.css';
-import './styles.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
