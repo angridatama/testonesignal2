b732450e691cd57f1d1467f4b7ce7e8b7f298adc
anggriawanmr <anggriawanmr@gmail.com>
2025-06-25 19:24:45 +0700

    Adding env

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ function App() {
   const [emailDisplay, setEmailDisplay] = useState(null);
   const emailRef = useRef(null);
 
+  const oneSignalAppId = process.env.ONESIGNAL_APP_ID;
+  const glideWebhookUrl = process.env.GLIDE_WEBHOOK_URL;
+
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
     const emailParam = params.get("email");
@@ -27,7 +30,7 @@ function App() {
     window.OneSignalDeferred.push(async function (OneSignal) {
       try {
         await OneSignal.init({
-          appId: "fcf28885-6e95-4401-8235-e8223ab2e898",
+          appId: oneSignalAppId,
           serviceWorkerPath: "OneSignalSDKWorker.js",
           serviceWorkerUpdaterPath: "OneSignalSDKUpdaterWorker.js",
           serviceWorkerParam: { scope: "/" },
@@ -63,21 +66,18 @@ function App() {
               setShowMarquee(false);
 
               try {
-                const response = await fetch(
-                  "https://go.glideapps.com/api/container/plugin/webhook-trigger/nyEQtv7S4N1E2SfxTuax/80a82896-f99a-40e0-a71c-c35eeb5f11a2",
-                  {
-                    method: "POST",
-                    headers: {
-                      "Content-Type": "application/json",
-                      Authorization:
-                        "Bearer fda014a4-0721-45b2-a1d8-4691500ae2da",
-                    },
-                    body: JSON.stringify({
-                      onesignalUserId: onesignalId,
-                      email: emailRef.current,
-                    }),
-                  }
-                );
+                const response = await fetch(glideWebhookUrl, {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                    Authorization:
+                      "Bearer fda014a4-0721-45b2-a1d8-4691500ae2da",
+                  },
+                  body: JSON.stringify({
+                    onesignalUserId: onesignalId,
+                    email: emailRef.current,
+                  }),
+                });
 
                 if (!response.ok) {
                   const text = await response.text();
